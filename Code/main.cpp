a99f0318fb0b9f27fbb71daff643d2d76ebd6ca7
bw55555 <brianwang2013@gmail.com>
2021-12-19 19:26:01 -0500

    hotfix

--- Code/main.cpp
+++ Code/main.cpp
@@ -112,5 +112,5 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     // make sure the viewport matches the new window dimensions; note that width and 
     // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);
+    glViewport(width/2 - height/4, 0, height/2, height);
 }
