16694abd5e541e71387cf26a000ec847b37a511b
kdports <48396657+kdports@users.noreply.github.com>
2021-12-23 22:57:28 -0500

    Revert "Revert "Merge branch 'kd-features'""
    This reverts commit 60b973476146b4645adf139f6907380a17de78cc.

--- Code/GameWindow.cpp
+++ Code/GameWindow.cpp
@@ -1,4 +1,5 @@
 #include "GameWindow.h"
+#include "EnemyBuilder.h"
 
 extern std::string PATH_START;
 
@@ -64,8 +65,6 @@ void GameWindow::initialize() {
     //dosmth with the player hitbox
     player = new Player(playerHitbox, playerTexture);
     
-    loadTexture(PATH_START + "resources/textures/TouhouFairy.png", &enemyTextures[0]);
-    
     //note that we may end up needing to put all of these into a spritesheet and use another function to choose the right texture when drawing
     loadTexture(PATH_START + "resources/textures/Bullet.png", &BulletSpawner::bulletPresetTextures[0]);
     loadTexture(PATH_START + "resources/textures/Knife.png", &BulletSpawner::bulletPresetTextures[1]);
@@ -103,6 +102,9 @@ void GameWindow::render() {
 }
 
 void GameWindow::update() {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
     //bomb!
     if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS)
         clearScreen();
@@ -225,8 +227,29 @@ void enemyTestFunc(Enemy* enemy) {
     }
     
     float dir = enemy->customFloats[0];
-    //enemy->move(glm::vec2(dir * spd, 0.0f));
+    enemy->move(glm::vec2(dir * spd, 0.0f));
     
+}
+
+void enemyFasterFunc(Enemy* enemy) {
+    //how to use void* like this?
+    if (enemy->customFloats.size() <= 0) {
+        std::cout << "Custom Floats not initialized" << std::endl;
+        return;
+    }
+    float xpos = enemy->getPos().x;
+    float spd = 20.0f;
+
+    if (xpos <= -800.0f) {
+        enemy->customFloats[0] = 1.0f;
+    }
+    if (xpos >= 800.0f) {
+        enemy->customFloats[0] = -1.0f;
+    }
+
+    float dir = enemy->customFloats[0];
+    enemy->move(glm::vec2(dir * spd, 0.0f));
+
 }
 
 void bulletSpawnerTestFunc(BulletSpawner* spawner) {
@@ -252,189 +275,15 @@ void bulletSpawnerTestFunc(BulletSpawner* spawner) {
 
 void Level1(GameLevel* level) {
     float ct = level->currTime;
-    if (ct == 30) {
-        Hitbox enemyHitbox;
-        enemyHitbox.type = HitboxType::Circle;
-        enemyHitbox.radius = 50.0f;
-        std::shared_ptr<Enemy> e = Enemy::makeEnemy(enemyHitbox, glm::vec2(0.0f, 500.0f), GameWindow::enemyTextures[0], enemyTestFunc);
-        e->customFloats.push_back(1.0f);
-        e->createBulletSpawner(glm::vec2(0, 0), bulletSpawnerTestSpinning);
-        /*
-        std::shared_ptr<Enemy> e2 = Enemy::makeEnemy(enemyHitbox, glm::vec2(-500.0f, 500.0f), enemyTextures[0], enemyTestFunc);
-        e2->customFloats.push_back(1.0f);
-        e2->createBulletSpawner(glm::vec2(0, 0), bulletSpawnerTestFunc);
-        std::shared_ptr<Enemy> e3 = Enemy::makeEnemy(enemyHitbox, glm::vec2(500.0f, 500.0f), enemyTextures[0], enemyTestFunc);
-        e3->customFloats.push_back(1.0f);
-        e3->createBulletSpawner(glm::vec2(0, 0), bulletSpawnerTestFunc);
-        */
-    }
-}
+    if (ct == 120) {
+        FairyBuilder* fairy = new FairyBuilder(); // Creates the FairyBuilder
+        DoppleBuilder* dopple = new DoppleBuilder(); // Creates the DoppleBuilder
+        EnemyBuildDirector director; //Creates the director
 
-void bulletSpawnerTestFunc2(BulletSpawner* spawner) {
-    if ((int)(spawner->currTime) % 1 == 0) {
-
-        std::shared_ptr<Bullet> bullet = spawner->spawnPreset(0, spawner->pos, testFunc2);
-        bullet->customFloats.push_back(10.0f);
-        float angle = glm::radians(60 * spawner->currTime);
-        glm::vec2 dir{ sin(angle), cos(angle) };
-        dir = glm::normalize(dir);
-        bullet->customFloats.push_back(spawner->pos.x);
-        bullet->customFloats.push_back(spawner->pos.y);
-        bullet->customFloats.push_back(dir.x);
-        bullet->customFloats.push_back(dir.y);
-        bullet->setRotation(dir);
-    }
-}
-
-void testFunc2(Bullet* b) {
-    //speed, centerX, centerY, directionX, directionY
-
-    glm::vec2 temp = glm::vec2(b->customFloats[1], b->customFloats[2]) - b->getPos();
-    glm::vec2 spinDir = glm::normalize(glm::vec2(temp.y, -temp.x));
-    glm::vec2 finalDir = glm::normalize(1.1f * spinDir + glm::vec2(b->customFloats[3], b->customFloats[4]));
-
-    if (b->currTime < 15) {
-        b->move(glm::vec2(0.0f, -1.0f) * b->customFloats[0]);
-    }
-    else {
-        b->move(finalDir * b->customFloats[0]);
-    }
-}
-
-void bulletSpawnerTestFuncDisplay(BulletSpawner* spawner) {
-    if ((int)(spawner->currTime) % 1 == 0) {
-
-        std::shared_ptr<Bullet> bullet = spawner->spawnPreset(1, spawner->pos, spinningDirectionalBulletDisplay);
-        bullet->customFloats.push_back(10.0f);
-        float angle = glm::radians(63.017 * spawner->currTime);
-        glm::vec2 dir{ sin(angle), cos(angle) };
-        dir = glm::normalize(dir);
-        bullet->customFloats.push_back(spawner->pos.x);
-        bullet->customFloats.push_back(spawner->pos.y);
-        bullet->customFloats.push_back(dir.x);
-        bullet->customFloats.push_back(dir.y);
-        bullet->setRotation(dir);
-    }
-}
-
-void spinningDirectionalBulletDisplay(Bullet* b) {
-    //speed, centerX, centerY, directionX, directionY
-
-    glm::vec2 temp = glm::vec2(b->customFloats[1], b->customFloats[2]) - b->getPos();
-    glm::vec2 spinDir = glm::normalize(glm::vec2(temp.y, -temp.x));
-    glm::vec2 finalDir = glm::normalize(spinDir * 1.05f + glm::vec2(b->customFloats[3], b->customFloats[4]));
-
-    if (b->currTime < 15) {
-        b->move(glm::vec2(b->customFloats[3], b->customFloats[4]) * b->customFloats[0]);
-    }
-    else {
-        b->move(finalDir * b->customFloats[0]);
-        b->setRotation(finalDir);
-    }
-}
-
-void bulletSpawnerTest3(BulletSpawner* spawner) {
-    if ((int)(spawner->currTime) % 1 == 0) {
-
-        std::shared_ptr<Bullet> bullet = spawner->spawnPreset(1, spawner->pos, testFunc3);
-        bullet->customFloats.push_back(10.0f);
-        float angle = glm::radians(64.513f * spawner->currTime);
-        glm::vec2 dir{ sin(angle), cos(angle) };
-        dir = glm::normalize(dir);
-        bullet->customFloats.push_back(spawner->pos.x);
-        bullet->customFloats.push_back(spawner->pos.y);
-        bullet->customFloats.push_back(dir.x);
-        bullet->customFloats.push_back(dir.y);
-        bullet->setRotation(dir);
-    }
-}
-
-void testFunc3(Bullet* b) {
-    //speed, centerX, centerY, directionX, directionY
-
-    glm::vec2 temp = glm::vec2(b->customFloats[1], b->customFloats[2]) - b->getPos();
-    glm::vec2 spinDir = glm::normalize(glm::vec2(temp.y, -temp.x)) * (1 + b->currTime/500);
-    glm::vec2 finalDir = spinDir + glm::vec2(b->customFloats[3], b->customFloats[4]);
-
-    if (b->currTime < 15) {
-        b->move(glm::vec2(b->customFloats[3], b->customFloats[4]) * b->customFloats[0]);
-    }
-    else if (b->currTime < 360) {
-        b->move(finalDir * b->customFloats[0]);
-        b->setRotation(finalDir);
-    }
-    else if (b->currTime == 360) {
-        b->customFloats.push_back(finalDir.x);
-        b->customFloats.push_back(finalDir.y);
-    }
-    else {
-        b->move(glm::vec2(b->customFloats[0] * glm::vec2(b->customFloats[5], b->customFloats[6])));
-    }
-}
-
-void bulletSpawnerTest4(BulletSpawner* spawner) {
-    if ((int)(spawner->currTime) % 1 == 0) {
-
-        std::shared_ptr<Bullet> bullet = spawner->spawnPreset(1, spawner->pos, spinningDirectionalBullet);
-        bullet->customFloats.push_back(10.0f);
-        float angle = glm::radians(64.513f * spawner->currTime);
-        glm::vec2 dir{ sin(angle), cos(angle) };
-        dir = glm::normalize(dir);
-        bullet->customFloats.push_back(spawner->pos.x);
-        bullet->customFloats.push_back(spawner->pos.y);
-        bullet->customFloats.push_back(dir.x);
-        bullet->customFloats.push_back(dir.y);
-        bullet->setRotation(dir);
-    }
-}
-
-void testFunc4(Bullet* b) {
-    //speed, centerX, centerY, directionX, directionY
-
-    glm::vec2 temp = glm::vec2(b->customFloats[1], b->customFloats[2]) - b->getPos();
-    glm::vec2 spinDir = glm::normalize(glm::vec2(temp.y, -temp.x));
-    glm::vec2 finalDir = glm::normalize(1.1f * spinDir + glm::vec2(b->customFloats[3], b->customFloats[4]));
-
-    if (b->currTime < 15) {
-        b->move(glm::vec2(b->customFloats[3], b->customFloats[4]) * b->customFloats[0]);
-    }
-    else {
-        b->move(glm::vec2((1 + b->currTime / 30) * b->customFloats[0] * finalDir));
-        b->setRotation(finalDir);
-    }
-
-    if (b->currTime == 100) {
-        b->destroy();
-    }
-}
-
-void bulletSpawnerTestSpinning(BulletSpawner* spawner) {
-    if ((int)(spawner->currTime) % 4 == 0) {
-        for (float offset = 0; offset < 360; offset += 72) {
-            float angle = glm::radians(3.0f * spawner->currTime + offset);
-            glm::vec2 dir{ cos(angle), sin(angle) };
-            std::shared_ptr<Bullet> bullet = spawner->spawnPreset(1, spawner->pos + dir, spinningDirectionalBullet);
-            bullet->customFloats.push_back(spawner->pos.x); //spin center x
-            bullet->customFloats.push_back(spawner->pos.y); //spin center y
-            bullet->customFloats.push_back(8.0f); //velocity (radius change)
-            bullet->customFloats.push_back(0.7f); //angle change
-            bullet->customFloats.push_back(0.02f); //acceleration
-            bullet->customFloats.push_back(0.00f); //spin acceleration
-        }
-    }
-}
-
-void spinningDirectionalBullet(Bullet* b) {
-    //centerX, centerY, radiusChange, angleChange, acceleration, spin acceleration
-    glm::vec2 center = glm::vec2(b->customFloats[0], b->customFloats[1]);
-    glm::vec2 radius = center - b->getPos();
-    float angle = 180 - glm::degrees(glm::orientedAngle(glm::normalize(radius), glm::vec2(1, 0)));
-    
-    float incrAngle = angle + b->customFloats[3];
-    float incrRadius = glm::length(radius) + b->customFloats[2] * (1 + b->currTime * b->customFloats[4]);
-    glm::vec2 radiusEx = glm::vec2(incrRadius * cos(glm::radians(incrAngle)), incrRadius * sin(glm::radians(incrAngle)));
-    b->move((radius + radiusEx) * (1 + b->currTime * b->customFloats[5]));
-    if (b->currTime == 100) {
-        b->destroy();
+        std::shared_ptr<Enemy> e = director.buildEnemy(fairy, glm::vec2(0.0f, 500.0f), enemyTestFunc); // Make a fairy at 0, 500
+        std::cout << e->getPos().y << std::endl;
+        std::shared_ptr<Enemy> e2 = director.buildEnemy(fairy, glm::vec2(500.0f, 100.0f), enemyFasterFunc); // Make another
+        std::cout << e2->getPos().y << std::endl;
+        std::shared_ptr<Enemy> e3 = director.buildEnemy(dopple, glm::vec2(500.0f, 500.0f), enemyTestFunc); // Make a doppleganger
     }
 }
