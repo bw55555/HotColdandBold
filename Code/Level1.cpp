bc29d24003132ba3072db72900dbe84dc9562388
bw55555 <brianwang2013@gmail.com>
2022-01-25 09:26:46 -0500

    hopefully fix that bug

--- Code/Level1.cpp
+++ Code/Level1.cpp
@@ -32,14 +32,14 @@ namespace Level {
         once(l) {
             std::shared_ptr<Enemy> e = BossEnemy::makeBossEnemy(300.0f, Hitbox::Circle(200), glm::vec2(0.0f, 500.0f), GameWindow::enemyTextures[6], minibossUFunc, glm::vec3(400.0f));
         }
-        delayClear(l, 30.0f, 10000.0f);
+        delayClear(l, 30.0f, 300.0f + 60_s * 2);
         L1Part4(l);
         L1Part5(l);
         wf(l, 120.0f) { GameWindow::Instance->clearScreen(); }
         once(l) {
             std::shared_ptr<Enemy> e = BossEnemy::makeBossEnemy(400.0f, Hitbox::Circle(200), glm::vec2(0.0f, 500.0f), GameWindow::enemyTextures[7], bossUFunc, glm::vec3(400.0f));
         }
-        delayClear(l, 30.0f, 10000.0f);
+        delayClear(l, 30.0f, 300.0f + 60_s * 4);
         
         wf(l, 300) {
             GameWindow::Instance->setWin(true);
--- Code/Level2.cpp
+++ Code/Level2.cpp
@@ -16,7 +16,7 @@ namespace Level {
         once(l) {
             std::shared_ptr<Enemy> e = BossEnemy::makeBossEnemy(300.0f, Hitbox::Circle(200.0f), glm::vec2(0.0f, 500.0f), GameWindow::enemyTextures[8], miniboss2UFunc, glm::vec3(400.0f));
         }
-        delayClear(l, 30.0f, 10000.0f);
+        delayClear(l, 30.0f, 30.0f + 60.0f * 60 * 2);
         delay(l, 180.0f);
         
         wf(l, 120.0f) { GameWindow::Instance->clearScreen(); }
@@ -24,7 +24,7 @@ namespace Level {
         once(l) {
             std::shared_ptr<Enemy> e = BossEnemy::makeBossEnemy(400.0f, Hitbox::Circle(200.0f), glm::vec2(0.0f, 500.0f), GameWindow::enemyTextures[9], boss2UFunc, glm::vec3(400.0f));
         }
-        delayClear(l, 30.0f, 10000.0f);
+        delayClear(l, 30.0f, 300.0f + 60.0f * 60 * 6);
         
         wf(l, 300) {
             GameWindow::Instance->setWin(true);
@@ -174,7 +174,7 @@ namespace Level {
             b->texture = BulletSpawner::bulletPresetTextures[1];
             b->setRotation(b->dir);
         }
-        after(b, 30) {
+        after(b, 60) {
             directionalBullet(b);
         }
     }
